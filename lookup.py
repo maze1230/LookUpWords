0547c822b69ec5b4e78cc0ec4e0887af32e8ea5c
maze1230 <maze1230@gmail.com>
2018-06-07 06:15:06 +0900

    b

--- lookup.py
+++ lookup.py
@@ -7,22 +7,49 @@ synonymURL = "https://od-api.oxforddictionaries.com:443/api/v1/entries/en/"
 synonym_API_key = None
 synonym_API_id = None
 
+def take_from_json(arg, cond, key=None):
+	res = []
+	if cond(arg, key):
+		res.append(arg)
+	if isinstance(arg, list):
+		for item in arg:
+			res += take_from_json(item, cond)
+	elif isinstance(arg, dict):
+		for k, v in arg.items():
+			res += take_from_json(v, cond, k)
+	return res
+
+def has_synonym_list(arg, key):
+	if isinstance(arg, list):
+		return key == "synonyms"
+
 def meaning(name):
 	req = requests.get(meaningURL+name)
 	soup = BeautifulSoup(req.text, 'lxml')
 	mean = soup.find_all("td", attrs={"class": "content-explanation"})
-	return mean[0].string.split('、')
+	res = []
+	for x in mean:
+		if not isinstance(x.string, str):
+			continue
+		res += x.string.split('、')
+	return res
 
 def synonym(name):
 	url = synonymURL+name+"/synonyms"
 	header = {"Accept": "application/json", "app_id": synonym_API_id, "app_key": synonym_API_key}
 	response = json.loads(requests.get(url, headers=header).text)
-	response = response["results"][0]["lexicalEntries"][0]["entries"][0]["senses"][0]["subsenses"][0]["synonyms"]
-	print(response)
+	response = take_from_json(response, has_synonym_list)
 	res = []
-	for idx, x in enumerate(response["synonyms"]):
-		if idx >= 10:
+	for x in response:
+		cnt = 0
+		for y in x:
+			s = y["text"]
+			if '-' in s or ' ' in s or s.lower() != s or s in res:
+				continue
+			res.append((s, meaning(s)))
+			cnt += 1
+			if cnt >= 5:
+				break
+		if len(res) >= 20:
 			break
-		res.append([x, meaning(x)])
-	print(res)
 	return res
--- main.py
+++ main.py
@@ -2,47 +2,24 @@ import csv
 import requests
 from bs4 import BeautifulSoup
 import json
+
 import lookup as lu
 import word
-
-def input_lint(): # Split numbers into list
-	return map(int, input().split(','))
-
-def choose_from_list(list_, num=20): # num -> max number of the item to show
-	ret = []
-	print()
-	for idx, item in enumerate(list_):
-		if idx >= num:
-			break
-		print(str(idx) + ": " + str(item))
-	print("Choose numbers(separate by ,) : ", end='')
-	x = input_lint()
-	for i in x:
-		ret.append(list_[i])
-	return ret
-
-def synonym_api_init():
-	try:
-		with open("./.synonym_key", mode='x') as f:
-			id_ = input("Synonym API ID(OxFordDictionary): ")
-			key = input("Synonym API Key                 : ")
-			f.writelines(id_+"\n")
-			f.writelines(key+"\n")
-	except FileExistsError:
-		with open("./.synonym_key") as f:
-			l = f.readlines()
-			l = [x[:-1] for x in l]
-			(id_, key) = (l[0], l[1])
-	lu.synonym_API_id = id_
-	lu.synonym_API_key = key
+import helper as hl
 
 if __name__ == '__main__':
-	synonym_api_init()
+	hl.synonym_api_init()
 	print("Look up meanings of the words.")
 	name = input("Input a word(-1 to end) -> ")
 
 	while name != "-1":
 		w = word.Word(name)
-		w.mean = choose_from_list(lu.meaning(name))
-		w.synonym = choose_from_list(lu.synonym(name))
+		try:
+			w.mean = hl.choose_from_list(lu.meaning(name))
+			w.synonym = hl.choose_from_list(lu.synonym(name))
+			w.synonym = list(map(hl.first, w.synonym))
+			print(w.synonym)
+		except LookupError:
+			print("There isn't this word.")
+			pass
 		name = input("Input a word(-1 to end) -> ")
